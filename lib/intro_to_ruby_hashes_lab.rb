0604a998e3dea908d2beac43bd3415dd20b71d17
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-29 03:09:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,17 @@
-def new_hash{
-}
+def new_hash
+
 end
 
 def my_hash
-  {:key => doe}
+ 
 end
 
 def pioneer
-  :name => 'Grace Hopper'
+  
  
 end
 
 
 def id_hash_generator(number)
-  :id => 3
+  
 end
